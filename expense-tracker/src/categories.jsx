0c8d8c5a72518527284cdcffefa4e4a9e25fb715
aromalkallingal5@gmail.com <aromalkallingal5@gmail.com>
2026-03-28 08:20:57 +0530

    learning useState

--- expense-tracker/src/categories.jsx
+++ expense-tracker/src/categories.jsx
@@ -4,79 +4,16 @@ import { useEffect } from "react";
 
 export default function Categories() {
 
-  useEffect(() => {
-  const saved = localStorage.getItem("totals");
-  if (saved) {
-    setTotals(JSON.parse(saved));
-  }
-}, []);
-
-useEffect(() => {
-  localStorage.setItem("totals", JSON.stringify(totals));
-}, [totals]);
-
-  const categories = ["groceries", "dineOut", "transport", "others"];
-
-  const [inputs, setInputs] = useState({
-    groceries: "",
-    dineOut: "",
-    transport: "",
-    others: ""
-  });
-
-  const [totals, setTotals] = useState({
-    groceries: 0,
-    dineOut: 0,
-    transport: 0,
-    others: 0
-  });
-
-  // handle input change
-  const handleChange = (category, value) => {
-    setInputs({
-      ...inputs,
-      [category]: value
-    });
-  };
-
-  // handle add button
-  const handleAdd = (category) => {
-    const value = Number(inputs[category]);
-
-    if (!value) return; // prevent empty or invalid input
-
-    setTotals({
-      ...totals,
-      [category]: totals[category] + value
-    });
-
-    // clear input after adding
-    setInputs({
-      ...inputs,
-      [category]: ""
-    });
-  };
-
+  const [count, setCount] = useState(0)
   return (
-    <div className="categories">
-      {categories.map((cat) => (
-        <div key={cat} className={`category-card ${cat}`}>
-          <h2>
-            {cat.charAt(0).toUpperCase() + cat.slice(1)}
-          </h2>
-
-          <input
-            type="number"
-            placeholder="Enter amount"
-            value={inputs[cat]}
-            onChange={(e) => handleChange(cat, e.target.value)}
-          />
-
-          <button onClick={() => handleAdd(cat)}>Add</button>
-
-          <p>Total: ₹ {totals[cat]}</p>
-        </div>
-      ))}
+    <div> 
+      <h1>count {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
-  );
+   
+  )
+
+ 
+
 }
